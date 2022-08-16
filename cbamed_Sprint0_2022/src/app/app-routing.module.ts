32769369bf6e6ed18d7929d533fb5bed38bdb995
Alba-Gaitan <albagaitan.fsk@gmail.com>
2022-08-16 00:25:56 -0300

    Actualizacion Angular

--- cbamed_Sprint0_2022/src/app/app-routing.module.ts
+++ cbamed_Sprint0_2022/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from 'src/app/pages/autenticacion/login/login.component';
+import {RegistroComponent} from 'src/app/pages/autenticacion/registro/registro.component';
+import {InicioComponent } from './Inicio/inicio/inicio.component';
+import { AcercaDeNosotrosComponent } from './pages/acerca-de-nosotros/acerca-de-nosotros.component';
 
-const routes: Routes = [];
+//defino las rutas path:'Inicio',component:nombre de componente
+const routes: Routes = [
+{path: 'AcercaDeNosotros', component:AcercaDeNosotrosComponent },
+{path: 'Login', component: LoginComponent},
+{path: 'Registro', component: RegistroComponent},
+{path: '', component: InicioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
